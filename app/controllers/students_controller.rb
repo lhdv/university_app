aa6dfa7c0b2562bbaa2834d82cbf536b8713a5e6
LH <lh@lhdv.net>
2020-10-26 19:25:35 -0300

    Add Edit/Update controller actions and views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   def index
     @students = Student.all  
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(name: params[:student][:name], email: params[:student][:email])
+    @student = Student.new(name: student_params[:name], email: student_params[:email])
     if @student.save
       flash[:notice] = 'Student created successfully'
       redirect_to students_path
@@ -21,6 +21,29 @@ class StudentsController < ApplicationController
       flash[:error] = 'Student cannot be created'
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @student.update(student_params)
+      flash[:notice] = 'Student updated successfully'
+      redirect_to student_path(@student)
+    else
+      flash[:error] = 'Student cannot be created'
+      render 'edit'
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
   
 end
